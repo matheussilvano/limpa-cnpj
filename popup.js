49875065915bf8857c8196e7f2defddf6bdbac76
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-07-10 15:17:10 -0300

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,15 +1,12 @@
 document.getElementById('cnpjInput').addEventListener('input', function() {
-    const cnpj = document.getElementById('cnpjInput').value;
-    const cnpjSemPontos = cnpj.replace(/[^\d]/g, '');
-    document.getElementById('resultado').textContent = cnpjSemPontos;
+    const cnpjs = document.getElementById('cnpjInput').value.split('\n');
+    const cnpjsSemPontos = cnpjs.map(cnpj => cnpj.replace(/[^\d]/g, '')).join('\n');
+    document.getElementById('resultado').textContent = cnpjsSemPontos;
     
-    // Copia automaticamente o CNPJ para a área de transferência
-    if (cnpjSemPontos.length === 14) { // Verifica se o CNPJ está completo
-      navigator.clipboard.writeText(cnpjSemPontos).then(function() {
-        console.log('CNPJ copiado para a área de transferência');
-      }).catch(function(error) {
+    // Copia automaticamente os CNPJs para a área de transferência
+    navigator.clipboard.writeText(cnpjsSemPontos).then(function() {
+        console.log('CNPJs copiados para a área de transferência');
+    }).catch(function(error) {
         console.error('Erro ao copiar para a área de transferência: ', error);
-      });
-    }
-  });
-  
+    });
+});
